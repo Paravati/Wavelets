9e4a4b587be6f97453f662ef6afeec07003442c1
Patrycja <pati.iwanek@gmail.com>
2021-08-21 20:39:55 +0200

    sombrero giph added; add more waves buttons to click

--- static/script.js
+++ static/script.js
@@ -1,19 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-//function myFunction() {
-//  document.getElementById("myDropdown").classList.toggle("show");
-//}
-//
-//// Close the dropdown if the user clicks outside of it
-//window.onclick = function(e) {
-//  if (!e.target.matches('.dropbtn')) {
-//  var myDropdown = document.getElementById("myDropdown");
-//    if (myDropdown.classList.contains('show')) {
-//      myDropdown.classList.remove('show');
-//    }
-//  }
-//}
-
 document.addEventListener('DOMContentLoaded', () => {
     let buttons=document.querySelectorAll('.button-wave');
     for (let ind = 0; ind < buttons.length; ind++)
@@ -29,8 +13,19 @@ function goto(but_name){
     case 'haar':
         document.location.href = 'haar.html';
         break;
+    case 'daub':
+        document.location.href = 'daubechie.html';
+        break;
+    case 'mex':
+        document.location.href = 'mexicanHat.html';
+        break;
+    case 'compl':
+        document.location.href = 'complexWavelets.html';
+        break;
+    case 'transform':
+        document.location.href = 'waveletTransform.html';
+        break;
     }
     console.log(but_name)
-//    document.location.href = 'morlet.html';
 }
 
